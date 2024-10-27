846f77e55cdd3c32008d02fe884fcf827feda2c8
Omar Tantawy <o.tantawy.o@gmail.com>
2024-10-27 12:01:18 +0300

    Challenge1Day3
    Bubble sort and insertion sort!

---
+++ Challenge1Day3/Challenge1Day3/Challenge1Day3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void BubbleSort(vector<int>& vec) {
+    int n = vec.size();
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (vec[j] > vec[j + 1]) {
+                swap(vec[j], vec[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+void InsertionSort(vector<int>& vec) {
+    int n = vec.size();
+    for (int i = 1; i < n; i++) {
+        int key = vec[i];
+        int j = i - 1;
+        while (j >= 0 && vec[j] > key) {
+            vec[j + 1] = vec[j];
+            j = j - 1;
+        }
+        vec[j + 1] = key;
+    }
+}
+
+void PrintVector(const vector<int>& vec) {
+    for (int value : vec)
+        cout << value << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> data = { 64, 34, 25, 12, 22, 11, 90 };
+
+    cout << "Unsorted Vector:" << endl;
+    PrintVector(data);
+
+    BubbleSort(data);
+
+    cout << "Sorted Vector using Bubble Sort:" << endl;
+    PrintVector(data);
+
+    vector<int> data2 = {64, 34, 25, 12, 22, 11, 90};
+
+    cout << "Unsorted Vector:" << endl;
+    PrintVector(data2);
+
+    InsertionSort(data2);
+
+    cout << "Sorted Vector using Bubble Sort:" << endl;
+    PrintVector(data2);
+
+    return 0;
+}
